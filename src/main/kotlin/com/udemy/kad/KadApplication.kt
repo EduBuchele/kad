048bde995eb786416bac5785c7f033b76ebc39c6
Eduardo <eduardo.buchele@gmail.com>
2026-02-09 10:51:49 -0300

    Init Commit

---
+++ src/main/kotlin/com/udemy/kad/KadApplication.kt
@@ -0,0 +1,11 @@
+package com.udemy.kad
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KadApplication
+
+fun main(args: Array<String>) {
+	runApplication<KadApplication>(*args)
+}
---
+++ src/test/kotlin/com/udemy/kad/KadApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.udemy.kad
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KadApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

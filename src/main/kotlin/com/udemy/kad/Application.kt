4dc2c78da0c24edb031faadf8db7a86e1cc5cdb6
Eduardo <eduardo.buchele@gmail.com>
2026-02-09 10:52:48 -0300

    Init Commit

--- src/main/kotlin/com/udemy/kad/Application.kt
+++ src/main/kotlin/com/udemy/kad/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KadApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<KadApplication>(*args)
+	runApplication<Application>(*args)
 }
